630ce60aa47c063e803cac3bbd77563bc3214f41
akulaakash17-byte <akulaakash17@gmail.com>
2026-04-06 22:03:21 +0530

    third commit

--- server.js
+++ server.js
@@ -66,4 +66,5 @@ app.get('/users', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
+    
 });
